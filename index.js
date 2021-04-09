3b7378000fca0c76be687c75f7c8beb7aca8742f
Mustafa Mustafa <mamustafa@gtcc.edu>
2021-04-09 19:36:26 -0400

    comments

---
+++ index.js
@@ -0,0 +1,53 @@
+class Node {
+    constructor(value) {
+        this.value = value
+        this.left - null
+        this.right = null
+    }
+}
+
+class BST {
+    constructor(value) {
+        this.root = new Node(value)
+        this.count = 0
+    }
+
+    size() {
+
+    }
+
+    insert() {
+
+    }
+
+    min() {
+
+    }
+
+    max() {
+
+    }
+
+    contains() {
+
+    }
+
+    // depth first search
+
+    // in-order
+    dfsPreOrder(){
+
+    }
+
+    //pre-order
+    dfsPreOrder(){
+        
+    }
+
+    // post-order 
+    dfsPreOrder(){
+        
+    }
+
+    // breadth first search
+}
