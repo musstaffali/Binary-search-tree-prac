5dace21ec2e6f3ad4db116d448af99febf6b50c5
Mustafa Mustafa <mamustafa@gtcc.edu>
2021-04-09 21:19:57 -0400

    traversing node

--- index.js
+++ index.js
@@ -24,23 +24,38 @@ class BST {
         const searchTree = node => {
             // if value < node.value, go left 
             if (value < node.value) {
-                
+                // if no left child, append new node
                 if (!node.left) {
                     node.left = newNode
-                } else {
+                } 
+                // if value > node.value, go right
+                else {
                     searchTree(node.left)
                 }
-
             }
             // if value > node.value, go right
             else if (value > node.value) {
-
+                if (!node.right) {
+                    node.right = newNode
+                }
+                else {
+                    searchTree(node.right)
+                }
             }
         }
+
+        searchTree(this.root)
     }
 
     min() {
+        let currentNode = this.root
 
+            // continue traversing left until no more children
+        while (currentNode.left) {
+            currentNode = currentNode.left
+        }
+
+        return currentNode.value
     }
 
     max() {
